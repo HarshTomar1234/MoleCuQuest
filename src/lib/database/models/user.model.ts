e620f1f54b5dbb2e0e6c7384efb155697384ba5b
Harsh Tomar <123729493+HarshTomar1234@users.noreply.github.com>
2025-08-25 01:23:50 +0530

    Add files via upload

---
+++ src/lib/database/models/user.model.ts
@@ -0,0 +1,49 @@
+import { Schema, model, models } from "mongoose";
+
+const UserSchema = new Schema(
+  {
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+    },
+    fullname: {
+      type: String,
+      unique: false,
+      required: false,
+    },
+    photo: {
+      type: String,
+      required: false,
+      default: "/images/user/user-01.png",
+    },
+    firstName: { type: String },
+    lastName: {
+      type: String,
+    },
+
+    password: {
+      type: String,
+      required: true,
+    },
+    isEmailVerified: {
+      type: Boolean,
+      default: false,
+    },
+    userBio: {
+      type: String,
+      default: "",
+    },
+    verificationToken: String,
+    verificationExpires: Date,
+    resetPasswordToken: String,
+    resetPasswordExpires: Date,
+  },
+  {
+    timestamps: true,
+  },
+);
+
+const User = models?.User || model("User", UserSchema);
+
+export default User;
